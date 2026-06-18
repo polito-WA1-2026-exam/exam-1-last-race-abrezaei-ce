8f8dc29654e3bfdbea789626db2d35cd9df928d9
abrezaei <abrezaei.ce@gmail.com>
2026-06-18 13:07:42 +0200

    feat: add leaderboard method

--- server/controllers/leaderboardController.js
+++ server/controllers/leaderboardController.js
@@ -1,5 +1,10 @@
+import { listLeaderboard } from "../repositories/leaderboardRepository.js";
+import { responseSuccess } from "../utils/response.js";
+
 const leaderboardController = {
-    list: async (req, res) => {}
+    list: async (req, res) => {
+        return responseSuccess(res, await listLeaderboard());
+    }
 }
 
 export default leaderboardController;
---
+++ server/repositories/leaderboardRepository.js
@@ -0,0 +1,15 @@
+import { queryAll } from "../utils/query.js";
+
+async function listLeaderboard() {
+    return await queryAll(`
+        SELECT user.username, MAX(game.score) as best_score
+        FROM users user
+        JOIN games game ON user.id = game.user_id
+        WHERE game.history IS NOT NULL AND game.score > 0
+        GROUP BY user.id
+        ORDER BY best_score DESC
+        LIMIT 10
+        `);
+}
+
+export { listLeaderboard }
